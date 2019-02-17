a5394df1ecfd5511b0a341fc5a2518f34babc782
Gugun <gugun.net21@gmail.com>
2019-02-17 17:35:07 +0700

    class Lingkaran

---
+++ PraUtsLingkaran/src/prautslingkaran/PraUtsLingkaran.java
@@ -0,0 +1,61 @@
+/*
+ //menghitung lingkaran
+
+1. Diketahui ada dua buah lingkaran, yakni ;
+  - C1 dengan jari-jari r1 dan, titik pusat di (x1, y1) serta 
+
+  - C2 dengan jari-jari r2, dan titik pusat di (x2, y2). 
+
+Buat program java yang dapat menginput nilai dari 
+ - C1 dan 
+ - C2, 
+
+lalu tentukan output sesuai dengan kondisi berikut:
+
+- Jika C2 di dalam C1, maka tulis "C2 ada di dalam C1"
+ - jika (saya) di dalam (rumah) maka tulis "saya ada di dalam rumah"//printlnnya
+
+- Jika C1 di dalam C2, maka tulis "C1 ada di dalam C2"
+ - jika (rumah) ada di dalam (gua) maka tulis " rumah ada di dalam gua"//printlnnya
+
+- Jika C1 dan C2 beririsan, maka tulis "C1 dan C2 beririsan"
+ -jika Gugun dan ika berdampingan, "gugun dan ika berdampingan"
+
+- Jika C1 dan C2 saling lepas, maka tulis "C1 dan C2 saling lepas"
+ - jika tikus dan kucing lepas, maka tulis "tikus dan kucing saling lepas"//printlnnya
+ */
+package prautslingkaran;
+
+import java.util.Scanner;
+
+public class PraUtsLingkaran {
+    class c1{
+        
+    }
+    public void c1(){
+        
+    }
+    public void c2(){
+        
+    }
+    // int c1=r1, 
+
+    public static void main(String[] args) {
+        
+        Scanner input = new  Scanner(System.in);
+        double phi = 3.14;
+        double r;
+        int c1;
+        
+        System.out.println("========================");
+        System.out.println("=======   Program  =====");
+        
+        System.out.print("Masukan C1");
+        //if (c1 > 100){
+        c1=input.nextInt();
+        System.out.println(": c2 ada di dalam c1");
+        
+        
+    }
+    }
+

9380d52f67b439fde3e593ce8172bfa946e2bc76
Gugun <gugun.net21@gmail.com>
2019-02-27 18:58:19 +0700

    Class Lingkaran pertemuan after UTS

---
+++ Interface/src/id/stmikamikbdg/kedua/Lingkaran.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.stmikamikbdg.kedua;
+
+/**
+ *
+ * @author WINDOWS 7X4
+ */
+public class Lingkaran implements BangunDatar{
+
+    Lingkaran(int i) {
+    }
+
+    @Override
+    public double Luas() {
+        System.out.println(" Luas Lingkaran :");
+        return 2*4;
+        
+    }
+
+    @Override
+    public double Keliling() {
+        System.out.println(" Keliling Lingkaran");
+        
+        return 5*3;
+    }
+    
+}

61f3324602f91acd1babe87f595e73be5c566f32
Gugun <gugun.net21@gmail.com>
2019-02-27 19:00:02 +0700

    Class Trapesium pertemuan after UTS

---
+++ Interface/src/id/stmikamikbdg/kedua/Trapesium.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.stmikamikbdg.kedua;
+
+/**
+ *
+ * @author WINDOWS 7X4
+ */
+public class Trapesium implements BangunDatar{
+
+    @Override
+    public double Luas() {
+        return 0;
+    }
+    
+    @Override
+    public double Keliling() {
+        return 0;
+    }
+    
+    
+}

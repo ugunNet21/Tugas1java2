d8457fc539f13ec1a8d9e3b6f74f330dc154d698
Gugun <gugun.net21@gmail.com>
2019-03-02 00:48:53 +0700

    tugas 10 jum'at waktu penggantian sabtu IO

---
+++ strim/src/idAMIK/macamBuku.java
@@ -0,0 +1,81 @@
+/*
+ tugas 2 after uts jum'at Maret 2019
+  IO
+ */
+package idAMIK;
+
+import java.util.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class macamBuku {
+
+    public static void addbuk() {
+        
+        try (FileOutputStream file = new FileOutputStream("AlamatBuku.dat", true)) {
+            Scanner input = new Scanner(System.in);
+
+            System.out.printf("Input Nama : ");
+            String Nama_ = input.nextLine() + "\n";
+            System.out.printf("Input Alamat: ");
+            String Alamat_ = input.nextLine() + "\n";
+            System.out.printf("Input No Telp: ");
+            String TEL = input.nextLine() + "\n";
+            System.out.printf("Input Email: ");
+            String Email = input.nextLine() + "\n";
+            file.write(Nama_.getBytes(Nama_));
+            file.write(Alamat_.getBytes(Alamat_));//
+            file.write(TEL.getBytes(TEL));
+            file.write(Email.getBytes(Email));//
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+    }
+    
+
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        
+        
+        do {
+            System.out.println("----------------------------------");
+            System.out.println("    WELCOME TO THE LYBLARY   I/O  ");
+            System.out.println("----------------------------------");
+            System.out.println("1. Tambah Data Buku/2. Tampilkan Data Buku/ 3. Keluar Apps");
+            System.out.println("Masukkan Pilihan = ");
+            int alif = input.nextInt();
+            
+            switch (alif) {
+                case 1:
+                    addbuk();
+                    break;
+                case 2:
+                    openbuk();
+                    break;
+                case 3:
+                    System.exit(00);
+                    break;
+                default:
+                    System.out.println("file not found/ Tidak ditemukan");
+                    break;
+            }
+            System.out.println();
+        } while (true);
+    }
+    public static void openbuk() throws IOException {
+        try (FileInputStream export = new FileInputStream("AlamatBuku.dat")) {
+            byte siklus;
+            while ((siklus = (byte) export.read()) != -1) {
+                System.out.println((char) siklus);
+            }
+            export.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("alamat tidak di input");
+        }
+    }
+
+}
